c8ff34816c4cd4a47016df96ad071daadf9c892e
fidzilalluqman <42725339+fidzilalluqman@users.noreply.github.com>
2018-10-29 21:58:24 +0700

    Add files via upload

--- class.php
+++ class.php
@@ -1,45 +1,58 @@
 <?php
-class Kalkulator{
+class mahasiswa{
     private $conn;
-
-    public function Kalkulator(){
+    public function mahasiswa(){
         $servername = "localhost";
-        $username   = "admin";
-        $password   = "1234";
-        $db         = "WebDasar";       
-        $this->conn = mysqli_connect($servername, $username, 
-                           $password, $db);                        
-    }    
-
+        $username   = "root";
+        $password   = "";
+        $db         = "6701174095";
+        $this->conn = mysqli_connect($servername, $username, $password, $db);
+    }
     public function tambah(){
-        $angka1 = $_POST['input1'];
-        $angka2 = $_POST['input2'];
-        $sql    = "INSERT INTO siswa(nama, nim) 
-                    VALUES ('$angka1','$angka2')";
-        mysqli_query($this->conn, $sql);        
-    }    
-    public function kurang(){        
-        $angka1 = $_POST['input1'];
-        $angka2 = $_POST['input2'];
-        $sql    = "DELETE FROM siswa WHERE nim=$angka2";        
-        mysqli_query($this->conn, $sql);
+        $nim = $_POST['input1'];
+        $nama = $_POST['input2'];
+        $sql    = "INSERT INTO tugas(nim, nama) VALUES ('$nim','$nama')";
+        if (mysqli_query($this->conn, $sql)) {
+            echo "berhasil";
+        }else {
+            echo mysqli_error($this->conn);
+        }
+    }
+    public function kurang(){
+        $nim = $_GET["nim"];
+        $sql    = "DELETE FROM tugas WHERE nim=$nim";
+        if (mysqli_query($this->conn, $sql)) {
+            echo "berhasil";
+        }else {
+            echo mysqli_error($this->conn);
+        }
     }
     public function bagi(){
-        $sql    = "SELECT * FROM siswa";        
+        $sql    = "SELECT * FROM tugas";
         return mysqli_query($this->conn, $sql);
-
+    }
+    public function update()
+    {
+        $nim = $_POST['input1'];
+        $nama = $_POST['input2'];
+        $sql    = "UPDATE tugas SET nama='$nama' WHERE nim='$nim'";
+        if (mysqli_query($this->conn, $sql)) {
+            echo "berhasil";
+        }else {
+            echo mysqli_error($this->conn);
+        }
     }
 }
-$operasi = $_POST["operasi"];
-$kalkulator = new Kalkulator();
+$operasi = isset($_POST["operasi"]) ? $_POST["operasi"] : "";
+$mahasiswa = new mahasiswa();
 if($operasi == "+")
-    $kalkulator->tambah();
-if($operasi == "-")
-    $kalkulator->kurang();
+    $mahasiswa->tambah();
+if(isset($_GET["nim"]))
+    $mahasiswa->kurang();
 if($operasi == "/"){
-    $result = $kalkulator->bagi();
+    $result = $mahasiswa->bagi();
     require_once("data.php");
 }
-    
-
-?>
+if(isset($_POST["edit"])) {
+    $kalkulator->update();
+}
--- edit.php
+++ edit.php
@@ -1,3 +1,6 @@
+<?php
+$nim = $_GET["nim"];
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,26 +10,20 @@
     <title>Document</title>
 </head>
 <body>
-    <table border="1" align="center"> 
+    <table border="1" align="center "> 
     <form action="Class.php" method="post">
-        <tr>    
+        <tr>
             <th>NIM</th>
-            <td><input type="text" name="input1"></td>
-            <td rowspan="2">    
-                <select name="operasi">
-                        <option value="+">+</option>
-                        <option value="-">-</option>
-                        <option value="*">*</option>
-                        <option value="/">/</option>
-                </select>
+            <td><input type="text" value="<?php echo $nim ?>" disabled>
+                <input type="hidden" name="input1" value="<?php echo $nim ?>">
             </td>
         </tr>
         <tr>
             <th>NAMA</th>
             <td><input type="text" name="input2"></td>
         </tr>
-        <tr>    
-        <td><input type="submit" value="Kirim"></td>
+        <tr>
+            <td><input type="submit" name="edit" value="Edit"></td>
         </tr>
     </form>
     </table>
